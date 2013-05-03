4ee5bb619fb47fe77d1815174f53957bb560cbc0
Dave Ortiz <daveortiz@gmail.com>
2013-05-03 13:23:54 -0400

    fixed

--- db/migrate/002_private_by_default.rb
+++ db/migrate/002_private_by_default.rb
@@ -1,10 +1,10 @@
 class AddPrivateToIssues < ActiveRecord::Migration
  
   def self.up
-    edit_column :issues, :private, :boolean, :default => true
+    change_column :issues, :private, :boolean, :default => true
   end
 
   def self.down
-    edit_column :issues, :private, :boolean, :default => false
+    change_column :issues, :private, :boolean, :default => false
   end
 end
